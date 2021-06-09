ef12487df54bc59169b51ce776d3cef8cbc1405b
Alejandro Jose Ruiz Estrada <83740410+AJRuiz22@users.noreply.github.com>
2021-06-08 20:32:45 -0600

    Plantilla general
    Es la plantilla general de la pagina de tienda de ropa mas el catalogo y el apartado de recomendaciones.

---
+++ shop/home.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+<head>
+	<title>Inicio</title>
+</head>
+<link rel="stylesheet" type="text/css" href="css/css.css">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+<meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1.0">
+<body>
+	<nav>
+		<label class="logo">Shop Online</label>
+			<ul>
+				<li><a class="active" href="">Home</a></li>
+				<li><a href="">Servicios</a></li>
+				<li><a href="">Contactos</a></li>
+				<li><a href="sobre.php">Sobre nosotros</a></li>
+				<li><a href="">Feedback</a></li>
+			</ul>
+	</nav>
+	<!-- Carrucel -->
+	<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+	  <div class="carousel-inner">
+	    <div class="carousel-item active">
+	      <img class="d-block w-100" src="imagenes/ropa.jpg" alt="First slide">
+	    </div>
+	    <div class="carousel-item">
+	      <img class="d-block w-100" src="imagenes/ropa1.jpg" alt="Second slide">
+	    </div>
+	    <div class="carousel-item">
+	      <img class="d-block w-100" src="imagenes/ropa3.jpg" alt="Third slide">
+	    </div>
+	  </div>
+	  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+	    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+	    <span class="sr-only">Previous</span>
+	  </a>
+	  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+	    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+	    <span class="sr-only">Next</span>
+	  </a>
+	</div>
+	<div class="cont2">
+		<h1 align="center">Shop Online</h1>
+		<hr align="center">
+		<p align="center">
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+		</p>
+	</div>
+	<div class="txt">
+		<p><h1 style="text-transform: uppercase; text-align: center;">Recomendados</h1></p>
+		<hr>
+	</div>
+	<div class="carrucel">
+		<div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+			<div class="carousel-inner">
+			    <div class="carousel-item active">
+			    	<img class="d-block w-100" src="imagenes/cred.jpg" alt="First slide">
+					<div class="carousel-caption d-none d-md-block">
+						<h5>Seguridad de pago</h5>
+						<p>Contamos con reembolso y seguro de pago</p>
+					</div>
+			    </div>
+			    <div class="carousel-item">
+			    <img class="d-block w-100" src="imagenes/env.jpg" alt="Second slide">
+					<div class="carousel-caption d-none d-md-block">
+						<h5>Con envio rapido y seguro</h5>
+						<p>Contamos con envio a todo el pais</p>
+					</div>
+			    </div>
+			    <div class="carousel-item">
+			    	<img class="d-block w-100" src="imagenes/prod/jpg" alt="Third slide">
+					<div class="carousel-caption d-none d-md-block">
+						<h5>Contamos con un amplio stock de ropa y productos</h5>
+						<p>De la mejor calidad, al mejor precio</p>
+					</div>
+			    </div>
+			  </div>
+			</div>
+	</div>
+
+	
+	
+	<div class="cont3">
+		<div class="prodcont">
+			<h3>Nombre</h3>
+			<img src="imagenes/1.jpg">
+			<h1>$50</h1>
+			<button class="button-add">Agregar</button>
+		</div>
+		<div class="prodcont">
+			<h3>Nombre</h3>
+			<img src="imagenes/2.jpg">
+			<h1>$50</h1>
+			<button class="button-add">Agregar</button>
+		</div>
+		<div class="prodcont">
+			<h3>Nombre</h3>
+			<img src="imagenes/3.jpg">
+			<h1>$50</h1>
+			<button class="button-add">Agregar</button>
+		</div>
+		<div class="prodcont">
+			<h3>Nombre</h3>
+			<img src="imagenes/4.jpg">
+			<h1>$50</h1>
+			<button class="button-add">Agregar</button>
+		</div>
+	</div>
+	<div class="pay">
+		<button class="button-chek">Pagar</button>
+	</div>
+
+
+	<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+
+
+
+
+
+
+</body>
+</html>
---
+++ shop/sobre.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Sobre nosotros</title>
+</head>
+<link rel="stylesheet" type="text/css" href="css/css.css">
+<meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1.0">
+<body background="imagenes/neon.png">
+	<nav>
+		<label class="logo">Shop Online</label>
+			<ul>
+				<li><a class="active" href="home.php">Home</a></li>
+				<li><a href="">Servicios</a></li>
+				<li><a href="">Contactos</a></li>
+				<li><a href="sobre.php">Sobre nosotros</a></li>
+				<li><a href="">Feedback</a></li>
+			</ul>
+	</nav>
+
+	<div class="sobre">
+		<p>
+			<h1>Sobre nosotros</h1>
+			<hr size="1px" color="white">
+			<br>
+			<div class="vision">
+				<p>
+					<h2>Mision</h2>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				</p>
+			</div>
+			<div class="mision">
+				<p>
+					<h2>Vision</h2>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				</p>
+			</div>
+		</p>
+	</div>
+</body>
+</html>
